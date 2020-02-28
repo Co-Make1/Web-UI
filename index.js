5ef1ed2d6ebb673d7d1d8fb14a86cdf696c327f3
Brian Clayton <finalb@projectfinalboss.com>
2020-02-27 23:38:41 -0500

    minor fix

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
+w3.addClass('.coMain', 'w3-center');
+w3.addClass('nav', 'w3-bar');
+w3.addClass('a', 'w3-bar-item');
 w3.addClass('.coHero', 'w3-hide-large w3-hide-medium');
 w3.addClass('.coHeroBig', 'w3-hide-small');
 w3.addClass('.coHeroBigCont', 'w3-hide-small');
-w3.addClass('.coPara', 'w3-cell w3-panel')
+w3.addClass('.coPara', 'w3-cell w3-panel');
