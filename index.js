aef504208b8930ec3101e9751ee08ee94af93f7f
Brian Clayton <finalb@projectfinalboss.com>
2020-02-27 23:17:34 -0500

    design flow come together

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 w3.addClass('.coHero', 'w3-hide-large w3-hide-medium');
 w3.addClass('.coHeroBig', 'w3-hide-small');
 w3.addClass('.coHeroBigCont', 'w3-hide-small');
+w3.addClass('.coPara', 'w3-cell w3-panel')
