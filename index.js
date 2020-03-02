137e6b4c6afb9c040fa493a43471c198769118c1
Brian Clayton <finalb@projectfinalboss.com>
2020-03-02 16:31:08 -0500

    major changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Index.html Flow
+
 
 w3.addClass('.coMain', 'w3-center');
 w3.addClass('nav', 'w3-bar');
@@ -6,3 +8,8 @@ w3.addClass('.coHero', 'w3-hide-large w3-hide-medium');
 w3.addClass('.coHeroBig', 'w3-hide-small');
 w3.addClass('.coHeroBigCont', 'w3-hide-small');
 w3.addClass('.coPara', 'w3-cell w3-panel');
+
+//About.html Flow
+
+w3.addClass('.aboutAvi, .aboutAvi2', 'w3-card w3-col s6 l4 ')
+w3.addClass('.aboutMain', 'w3-row')
